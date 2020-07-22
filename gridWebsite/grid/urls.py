ff38dcf4409941a92fe1cf363b952731cf0f6b5f
Romel <omelzg@gmail.com>
2020-07-22 14:50:25 +1000

    added index.html, moved home.html contents, created a home viewwith views.home

--- gridWebsite/grid/urls.py
+++ gridWebsite/grid/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', index.home),
+    path('home', views.home),
     path('simple/', views.simple),
     path('simpleweekly/', views.simpleweekly),
 	path('simpleweeklygrid/', views.simpleweeklygrid),
