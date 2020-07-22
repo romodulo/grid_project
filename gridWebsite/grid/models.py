6620a0bf327271643bdb4a2e5f4e990a8325ec1f
Romel <omelzg@gmail.com>
2020-07-23 01:47:09 +1000

    added Player model and Block model

--- gridWebsite/grid/models.py
+++ gridWebsite/grid/models.py
@@ -1,26 +1,5 @@
 from django.db import models
 
-class TennisPlayer(models.Model):
-	name = models.CharField(max_length=200, null=True)
-
-	def __str__(self):
-		return self.name
-
-class TimeBooked(models.Model):
-	# make 48 categories
-	# if 1 category is taken
-	# do not allow Player to book under
-	# this category
-	name = models.CharField(max_length=200, null=True)
-
-	def __str__(self):
-		return self.name
-
-class Booking(models.Model):
-	booking_name = models.ForeignKey(TennisPlayer, null=True, on_delete=models.SET_NULL)
-	booking_time = models.ForeignKey(TimeBooked, null=True, on_delete=models.SET_NULL)
-	# I need a date created, time created (especially)
-
 class Player(models.Model):
 	name = models.CharField(max_length=10)
 
