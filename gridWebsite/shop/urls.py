4aed7d1a28dd76e0770bb8b7d39823cb4e793b40
Romel <omelzg@gmail.com>
2020-07-24 19:58:35 +1000

    saving ...will create a new app for idea creation

--- gridWebsite/shop/urls.py
+++ gridWebsite/shop/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('shop/', views.shop_home ),
     path('shop/home', views.shop_home ),
+    path('shop/home/v1', views.shop_home_v1 ),
 ]
--- gridWebsite/shop/views.py
+++ gridWebsite/shop/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Log
 
+# These are a work in progress:
 def shop_home(request):
 	obj = Log.objects.all()
-
-
-
-
 	context = {"objects": obj}
-	
 	return render(request, 'shop/shop_home.html', context)
+
+
+# Below are all my archives
+def shop_home_v1(request):
+	obj = Log.objects.all()
+	context = {"objects": obj}
+	return render(request, 'shop/shop_home_v1.html', context)
