0c8e082019f48dbbfa3d3a675e01cc0c4b71966a
Romel <omelzg@gmail.com>
2020-07-24 16:17:09 +1000

    added str representions for all Models

--- gridWebsite/shop/models.py
+++ gridWebsite/shop/models.py
@@ -3,19 +3,33 @@ from django.db import models
 class Court(models.Model):
 	court_number = models.IntegerField()
 
+	def __str__(self):
+		return self.court_number
+
 class Time(models.Model):
 	court_time = models.TimeField()
+
+	def __str__(self):
+		return self.court_time
 
 class Month(models.Model):
 	name 		= models.CharField(max_length=10)
 	month_date 	= models.DateField()
 
+	def __str__(self):
+		return self.name
+
 class Player(models.Model):
 	name 		= models.CharField(max_length=20)
+	def __str__(self):
+		return self.name
 
 class WhatsappNum(models.Model):
 	number 		= models.IntegerField()
 	name 		= models.ForeignKey(Player, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.number
 
 class Log(models.Model):
 	court 		= models.ForeignKey(Court, on_delete=models.CASCADE)
@@ -23,3 +37,7 @@ class Log(models.Model):
 	player1 	= models.ForeignKey(Player, on_delete=models.CASCADE)
 	month_date	= models.ForeignKey(Month, on_delete=models.CASCADE)
 	guest		= models.CharField(max_length=10)
+	log_created	= models.DateField(auto_now_add=True)
+
+	def __str__(self):
+		return self.log_created
